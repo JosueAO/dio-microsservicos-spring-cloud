ce0c8e4d8bd23c29c883972c285c3ed74abf5a14
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-09-09 11:25:42 -0300

    Contoller, Model, Repository e correções de conflitos: Depedencias e JDK

--- product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/config/ElasticSearchConfig.java
+++ product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/config/ElasticSearchConfig.java
@@ -1,14 +1,18 @@
-package one.digitalinnovation.experts.productcatalog.config;
+package digitalinnovation.one.experts.productcatalog.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchEntityMapper;
+import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "one.digitalinnnovation.productcatalog.repository")
+@EnableElasticsearchRepositories(basePackages = "digitalinnovation.one.experts.productcatalog.repository")
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
     @Override
@@ -19,4 +23,13 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
         return RestClients.create(clientConfiguration).rest();
     }
 
+    @Bean
+    @Override
+    public EntityMapper entityMapper() {
+        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(elasticsearchMappingContext(),
+                new DefaultConversionService());
+        entityMapper.setConversions(elasticsearchCustomConversions());
+
+        return entityMapper;
+    }
 }
